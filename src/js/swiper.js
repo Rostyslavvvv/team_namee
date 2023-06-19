43a25add60f875878c33105e9331b336e439a644
Jo1ner <semdimas@gmail.com>
2023-06-19 09:51:13 +0300

    fix-2

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,13 +1,27 @@
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
-  loop: true,
+  loop: false,
+  speed: 1000,
   slidesPerView: 1,
+  centeredSlide: true,
+  
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    type: 'bullets',
     clickable: true,
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 18,
+       },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 18,} 
   },
 
   // Navigation arrows
