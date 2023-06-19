02dca5b687d28ce28824d7910491dd7ba0a488f1
KulibabaYuliia <kulibaba120@gmail.com>
2023-06-19 21:13:20 +0300

    fix swiper
    Delete dublicate links in index
    fix swiper

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,36 +1,36 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    slidesPerView: 1,
-    spaceBetween: 10,
-    // Responsive breakpoints
-    breakpoints: {
-      // when window width is >= 320px
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 28,
-      },
-      1158: {
-        slidesPerView: 3,
-        spaceBetween: 28,
-      },
+const swiper = new Swiper('.swiper_1', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 28,
     },
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
+    1158: {
+      slidesPerView: 3,
+      spaceBetween: 28,
     },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
