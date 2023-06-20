52e8f45fe6546e6fbbc72144ce74950dd4a70439
Rostyslavvvv <129362984+Rostyslavvvv@users.noreply.github.com>
2023-06-20 10:55:57 +0200

    modal_1

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,14 +1,15 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+  const openModalBtns = document.querySelectorAll("[data-modal-open]");
+  const closeModalBtn = document.querySelector("[data-modal-close]");
+  const modal = document.querySelector("[data-modal]");
+
+  openModalBtns.forEach((btn) => {
+    btn.addEventListener("click", toggleModal);
+  });
+
+  closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    modal.classList.toggle("is-hidden");
+  }
+})();
